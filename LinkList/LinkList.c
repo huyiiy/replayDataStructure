bd1fd4719bade45aa8a33316e96b62cb24966c2c
HU <1728995476@qq.com>
2023-12-20 11:26:31 +0800

    [修改]完善链表的遍历操作

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -109,13 +109,13 @@ int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTYPE val)
 /* 链表头删 */
 int LinkListHeadDel(LinkList * pList)
 {
-
+    
 }
 
 /* 链表尾删 */
 int LinkListTaiDel(LinkList * pList)
 {
-
+    
 }
 
 /* 链表指定位置删 */
@@ -156,5 +156,29 @@ int LinkListDestroy(LinkList * pList)
 /* 链表遍历接口 */
 int LinkListForeach(LinkList * pList)
 {
-
+    int ret = 0;
+    if (pList == NULL)
+    {
+        return NULL_PTR;
+    }
+#if 0
+    /* travelNode为虚拟头结点 */
+    LinkNode * travelNode = pList->head;
+      while (travelNode->next != NULL)
+    {
+        travelNode = travelNode->next;
+        printf("travelNode->data:%d\n", travelNode->data);
+    }
+#else
+    /* travelNode 指向链表第一个元素 */
+    LinkNode * travelNode = pList->head->next;
+      while (travelNode != NULL)
+    {    
+        printf("travelNode->data:%d\n", travelNode->data);
+        travelNode = travelNode->next;
+    }
+#endif
+    return ret;
+    
+    
 }
