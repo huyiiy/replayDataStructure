08c59df44b52c55726a35cd3b5a7cff8d84622d6
HU <1728995476@qq.com>
2023-12-20 10:13:27 +0800

    [修改]完成函数的形参，并在源文件定义

--- LinkList/LinkList.h
+++ LinkList/LinkList.h
@@ -24,34 +24,34 @@ typedef struct LinkList
 } LinkList;
 
 /* 链表初始化 */
-int LinkListInit();
+int LinkListInit(LinkList ** pList);
 
 /* 链表头插 */
-int LinkListHeadInsert();
+int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val);
 
 /* 链表尾插 */
-int LinkListTailInsert();
+int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val);
 
 /* 链表指定位置插入 */
-int LinkListAppointPosInsert();
+int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTYPE val);
 
 /* 链表头删 */
-int LinkListHeadDel();
+int LinkListHeadDel(LinkList * pList);
 
 /* 链表尾删 */
-int LinkListTaiDel();
+int LinkListTaiDel(LinkList * pList);
 
 /* 链表指定位置删 */
-int LinkListDelAppointPos();
+int LinkListDelAppointPos(LinkList * pList, int pos);
 
 /* 链表删除指定的数据 */
-int LinkListDelAppoint();
+int LinkListDelAppointData(LinkList * pList, ELEMENTTYPE val);
 
 /* 获取链表的长度 */
-int LinkListGetLength();
+int LinkListGetLength(LinkList * pList, int * pSize);
 
 /* 链表的销毁 */
-int LinkListDestroy();
+int LinkListDestroy(LinkList * pList);
 
 
 #endif
