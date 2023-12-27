74066551072e6a50b1b209a6375aa4d8238f1e0b
HU <1728995476@qq.com>
2023-12-27 11:26:51 +0800

    [修改]完善二叉搜索树的后继结点

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -131,10 +131,26 @@ static BSTreeNode * bstreeNodePreDecessor(BSTreeNode *node)
 }    
 
 /* 获取当前结点的后继结点 */
+/* 后继结点：中序遍历时的比当前结点大的第一个结点 */
 static BSTreeNode * bstreeNodeSuccessor(BSTreeNode *node)
 {
-    
+    if (node->right != NULL)
+    {
+        BSTreeNode *travelNode = node->right;
+        while (travelNode->left != NULL)
+        { 
+            travelNode = travelNode->left;
+        }
+        return travelNode;       
+    }
+    /* 程序执行到这个地方 说明一定没有右子树。 那就需要向父结点找 */
+    while (node->parent != NULL && node == node->parent->right)
+    {
+        node = node->parent;
+    }
+    return node->parent;
 } 
+
 /* 创建结点 */
 static BSTreeNode *createBSTreeNewNode(ELEMENTTYPE val, BSTreeNode * parentNode)
 {
