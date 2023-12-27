e3c13f1079f5e3bb3bfd54845aca1b49aa968dd4
HU <1728995476@qq.com>
2023-12-27 10:49:28 +0800

    [修改]完善二叉搜索树的前驱结点

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -104,29 +104,37 @@ static int binarySearchTreeNodeIsLeaf(BSTreeNode *node)
 }
 
 /* 获取当前结点的前驱结点 */
+/* 中序遍历搭到结点的前一个结点 */
 static BSTreeNode * bstreeNodePreDecessor(BSTreeNode *node)
 {
-    /* 度为2 */
-    if (binarySearchTreeNodeHasTwochildren(node))
-    {
-        /* 度为2， 前驱结点是在左子树的*/
-        BSTreeNode *travelNode = node->left;
-        while (travelNode->right != NULL)
+      if (node->left != NULL)
         {
-            /*  */
-            travelNode = travelNode->right;
-        }
-        return travelNode;
-        
-    }
-    /* c程序到这个地方一定是度为1 或 度为0 的*/
+            /* 前驱结点是在左子树的右子树的右子树.. */
+            BSTreeNode *travelNode = node->left;
+            while (travelNode->right != NULL)
+            {
+                /*  */
+                travelNode = travelNode->right;
+            }
+            return travelNode;    
+        }   
 
-    /* 度为1 */
-    /* 度为0 */
-}
+        /* 程序执行到这个地方 说明一定没有左子树。 那就需要向父结点找 */
+        while (node->parent != NULL && node == node->parent->left)
+        {
+            node = node->parent;
+        }
+        /* node->parent == NULL.*/
+        /* node == node->parent->right.*/
+        return node->parent;
+        
+}    
 
 /* 获取当前结点的后继结点 */
-static BSTreeNode * bstreeNodeSuccessor(BSTreeNode *node); 
+static BSTreeNode * bstreeNodeSuccessor(BSTreeNode *node)
+{
+    
+} 
 /* 创建结点 */
 static BSTreeNode *createBSTreeNewNode(ELEMENTTYPE val, BSTreeNode * parentNode)
 {
